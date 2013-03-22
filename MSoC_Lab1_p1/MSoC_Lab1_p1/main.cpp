dc0b3cf09f2bad7f0f6356db600c462b0959df31
Vince <vince@cardinalblue.com>
2013-03-23 01:01:02 +0800

    add fifo_fir into Lab_p1

--- MSoC_Lab1_p1/MSoC_Lab1_p1/main.cpp
+++ MSoC_Lab1_p1/MSoC_Lab1_p1/main.cpp
@@ -1,18 +1,31 @@
-//
-//  main.cpp
-//  MSoC_Lab1_p1
-//
-//  Created by Lee Haw on 13/3/23.
-//  Copyright (c) 2013年 Lee Haw. All rights reserved.
-//
+//BEGIN main.cpp
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//See fifo_fir.h for more information
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#include "systemc.h"
+#include <iostream>
+using std::cout;
+using std::endl;
 
-int sc_main (int argc, char * argv[]) {
-			
-    // insert code here...
-    std::cout << "Hello, SystemC!\n";
-    sc_start();
-    return 0;
+#include <systemc.h>
+#include "fifo_fir.h"
+
+unsigned errors = 0;
+char* simulation_name = "fifo_fir";
+
+int sc_main(int argc, char* argv[]) {
+    //sc_set_time_resolution(1,SC_PS);
+    //sc_set_default_time_unit(1,SC_NS);
+    fifo_fir sc_fifo_ex_i("sc_fifo_ex_i", "/Users/leehaw/Documents/MSoC_workspace/Lab1/code/Sample_Code/fifo_fir//fifo_fir.cfg");
+    cout << "INFO: Starting "<<simulation_name<<" simulation" << endl;
+    if (errors == 0) sc_start();
+    cout << "INFO: Exiting "<<simulation_name<<" simulation" << endl;
+    cout << "INFO: Simulation " << simulation_name
+        << " " << (errors?"FAILED":"PASSED")
+        << " with " << errors << " errors"
+        << endl;
+    return errors?1:0;
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//END $Id: main.cpp,v 1.1 2003/11/24 17:21:26 dcblack Exp $
